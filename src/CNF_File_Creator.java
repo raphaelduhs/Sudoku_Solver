75499bb2dfcd76106f0e69788be1429f0954e23d
raphaelduhs <raphael.duhs@gmail.com>
2019-05-10 16:18:59 +0200

    Input over Console for the Creator

--- src/CNF_File_Creator.java
+++ src/CNF_File_Creator.java
@@ -1,4 +1,53 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
 // creates a CNF File for a x*x Sudoku grid
-public class CNF_File_Creator {
+public class CNF_File_Creator  {
+
+
+    public static void main(String[] args)   {
+
+        int size = 1;
+
+    //get size as a Input from console
+     try {
+         size = inputSize();
+
+     } catch (IOException e) {
+         //no input
+     }
+
+
+
+
+
+
+
+
+    }
+
+
+
+    public static int inputSize() throws IOException {
+
+        System.out.println("Geben sie eine Zahl zwischen 2 und 6 ein");
+        //read the size of the Sudoku
+        BufferedReader reader =
+                new BufferedReader(new InputStreamReader(System.in));
+
+        // Reading data using readLine
+       int number = Integer.parseInt(reader.readLine());
+
+       return number;
+
+
+    }
+
+
+
+
+
+
 
 }
